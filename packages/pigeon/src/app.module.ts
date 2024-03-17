49ccbf5b3290ca88c87bc6dbec978499e362a82e
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-03-17 17:44:10 -0600

    Added database configuration to pigeon package

--- packages/pigeon/src/app.module.ts
+++ packages/pigeon/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { config } from './config';
+import { databaseConfiguration } from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: config,
+    }),
+    TypeOrmModule.forRoot(databaseConfiguration())
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/pigeon/src/config/database.ts
@@ -0,0 +1,11 @@
+import { DataSourceOptions } from "typeorm";
+
+export const databaseConfiguration = (): Partial<DataSourceOptions> => ({
+  type: process.env.DATABASE_TYPE as any,
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT!),
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  synchronize: false
+})
