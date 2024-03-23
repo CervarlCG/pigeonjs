58830e080d9539552e38ae6522e71ad04f26bb6a
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-03-23 01:53:36 -0600

    Fix: Auth module was added to app module and missing user entity was added to auth module

--- packages/pigeon/src/models/auth/auth.module.ts
+++ packages/pigeon/src/models/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET_KEY,
