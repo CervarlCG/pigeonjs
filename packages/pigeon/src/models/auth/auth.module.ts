ee376d1453c161f1fead604dc6e2ab227489aca3
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-03-23 01:45:10 -0600

    Added @nestjs/jwt dependency and implemented on auth service signIn method

--- packages/pigeon/src/models/auth/auth.module.ts
+++ packages/pigeon/src/models/auth/auth.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '60s' }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService]
 })
--- packages/pigeon/src/models/auth/auth.service.ts
+++ packages/pigeon/src/models/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
-import { ResourceNotFoundException, UnauthorizedException } from 'src/common/exceptions';
+import { UnauthorizedException } from 'src/common/exceptions';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private jwtService: JwtService,
   ){}
 
   async signIn(email: string, password: string) {
@@ -16,6 +18,10 @@ export class AuthService {
     if( !user || !this.userService.verifyPassword(user.password, password) ) 
       throw new UnauthorizedException("Authentication failed. Please check your login details and try again.");
 
-    return this.userService.toDto(user);
+    const payload = {sub: user.id, email: user.email};
+
+    return {
+      accessToken: await this.jwtService.signAsync(payload)
+    }
   }
 }
