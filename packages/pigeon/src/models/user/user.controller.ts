f235bf3d2abaac9d080433d12bace68f81553c6f
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-03-24 17:02:38 -0600

    docs: Added documentation to user module

--- packages/pigeon/src/models/user/user.controller.ts
+++ packages/pigeon/src/models/user/user.controller.ts
@@ -4,6 +4,11 @@ import { UserService } from './user.service';
 import { UnauthorizedException } from 'src/common/exceptions/system';
 
 
+/**
+ * UserController is a controller class that handles user-related requests.
+ * It uses the UserService to interact with the user data.
+ * All endpoints in this controller are protected by the JwtAuthGuard.
+ */
 @Controller('users')
 @UseGuards(JwtAuthGuard)
 export class UserController {
@@ -11,6 +16,11 @@ export class UserController {
     private userService: UserService
   ) {}
 
+  /**
+   * Returns the logged-in user's information.
+   * @param req - The request object containing the user's email.
+   * @returns The user's DTO.
+   */
   @Get("me")
   async getLoggedInUser(@Request() req: any) {
     const user = await this.userService.findOne(req.user.email);
--- packages/pigeon/src/models/user/user.service.ts
+++ packages/pigeon/src/models/user/user.service.ts
@@ -3,22 +3,35 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import * as bcrypt from "bcrypt";
 import { DeleteOptions, FindOptions } from 'src/common/interfaces/repository';
 import { getDeletedAtWhereClausule } from 'src/common/helpers/repository';
 import { ResourceConflictException, UnauthorizedException } from 'src/common/exceptions/system';
+import * as bcrypt from "bcrypt";
 
+/**
+ * Service for managing user-related operations.
+ */
 @Injectable()
 export class UserService {
+  /**
+   * Constructor for UserService.
+   * @param userRepository - The repository for user entity.
+   */
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
 
+  /**
+   * Creates a new user.
+   * @param userInput - The user input DTO.
+   * @throws UnauthorizedException - If the user was previously deleted.
+   * @throws ResourceConflictException - If an user with the given email already exists.
+   */
   async create(userInput: CreateUserDto) {
     const existingUser = await this.findOne(userInput.email, {allowDeleted: true});
 
-    // TODO: What to do when a previously deleted user try to create same account again? 
+    // TODO: What to do when a previously deleted user try to create same account again?
     if( existingUser && existingUser.deletedAt !== null )
       throw new UnauthorizedException("This account was previously deleted. Please contact site owner");
     else if (existingUser)
@@ -28,6 +41,11 @@ export class UserService {
     return await this.userRepository.save(user);
   }
 
+  /**
+   * Finds one user by email.
+   * @param email - The email of the user to find.
+   * @param options - Options for finding the user.
+   */
   async findOne( email: string, options: FindOptions = {} ) {
     return this.userRepository.findOne({where: {
       email, 
@@ -35,6 +53,11 @@ export class UserService {
     }});
   }
 
+  /**
+   * Deletes a user.
+   * @param id - The id of the user to delete.
+   * @param options - Options for deleting the user.
+   */
   async delete( id: number, options: DeleteOptions = {} ) {
     if( options.hardDelete !== true )
       this.userRepository.update({ id }, { deletedAt: new Date() });
@@ -42,18 +65,36 @@ export class UserService {
       this.userRepository.delete({id});
   }
 
+  /**
+   * Hashes a password.
+   * @param password - The password to hash.
+   */
   async hashPassword(password: string) {
     return bcrypt.hash(password, 10);
   }
 
+  /**
+   * Verifies a password against a hashed password.
+   * @param hashedPassword - The hashed password.
+   * @param plainPassword - The plain password to verify.
+   */
   async verifyPassword(hashedPassword: string, plainPassword: string) {
     return bcrypt.compare(plainPassword, hashedPassword);
   }
 
+  /**
+   * Sets a refresh token for a user.
+   * @param userId - The id of the user.
+   * @param refreshToken - The refresh token to set.
+   */
   async setRefreshToken(userId: number,refreshToken: string | null) {
     await this.userRepository.update( {id: userId}, { refreshToken });
   }
 
+  /**
+   * Converts a user entity to a user DTO.
+   * @param user - The user entity to convert.
+   */
   toDto(user: User) {
     return { id: user.id, email: user.email, firstName: user.firstName, lastName: user.lastName };
   }
