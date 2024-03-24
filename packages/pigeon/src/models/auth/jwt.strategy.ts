e50d0e76bef6ea2b4b37f84574f69fbe83dec512
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-03-23 20:52:58 -0600

    Docs: Added documentation to auth module and fixed password verification on validate user

--- packages/pigeon/src/models/auth/jwt.strategy.ts
+++ packages/pigeon/src/models/auth/jwt.strategy.ts
@@ -2,6 +2,11 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
+/**
+ * JwtStrategy class that extends PassportStrategy with JWT authentication.
+ * It uses the JWT token from the authorization header as a bearer token
+ * and validates it using the secret key from environment variables.
+ */
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
@@ -9,10 +14,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET_KEY
-    })
+    });
   }
 
-  async validate(payload: any ) {
-    return { id: payload.sub, email: payload.email }
+  /**
+   * Validates the payload of the JWT token and returns the user object.
+   * @param payload - The payload of the JWT token.
+   * @returns The user object with id and email properties.
+   */
+  async validate(payload: any) {
+    return { id: payload.sub, email: payload.email };
   }
 }
