1b9ddb0867b5b6f52b70f277e016cebcf7fe951e
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-04-13 20:14:00 -0600

    Updated workspaces middleware to get user & owner relations columns from user service

--- packages/pigeon/src/models/workspace/workspace.middleware.ts
+++ packages/pigeon/src/models/workspace/workspace.middleware.ts
@@ -2,10 +2,14 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { WorkspaceService } from './workspace.service';
 import { AppRequest } from 'src/common/interfaces/http';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class WorkspaceMiddleware implements NestMiddleware {
-  constructor(private readonly workspaceService: WorkspaceService) {}
+  constructor(
+    private readonly workspaceService: WorkspaceService,
+    private readonly userService: UserService,
+  ) {}
 
   async use(req: AppRequest, res: Response, next: NextFunction) {
     const workspaceId = req.params.workspaceId;
@@ -15,8 +19,8 @@ export class WorkspaceMiddleware implements NestMiddleware {
         {
           relations: { users: true, owner: true },
           select: {
-            users: { id: true, email: true, role: true },
-            owner: { id: true, email: true, role: true },
+            users: this.userService.getRelationColums(),
+            owner: this.userService.getRelationColums(),
           },
         },
       );
