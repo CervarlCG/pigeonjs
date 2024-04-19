9bc2c90f46532bb5061fadc62f641c5076d7e333
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-04-18 20:09:03 -0600

    Fix: Middleware implementation on channel module

--- packages/pigeon/src/models/channels/channel.module.ts
+++ packages/pigeon/src/models/channels/channel.module.ts
@@ -1,6 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { WorkspaceMiddleware } from '../workspace/workspace.middleware';
 import { PaginationService } from '../pagination/pagination.service';
 import { Channel } from './entities/channel.entity';
 import { ChannelService } from './channel.service';
@@ -9,6 +8,7 @@ import { WorkspaceService } from '../workspace/workspace.service';
 import { Workspace } from '../workspace/entities/workspace.entity';
 import { UserService } from '../user/user.service';
 import { User } from '../user/entities/user.entity';
+import { ChannelMiddleware } from './channel.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Channel, Workspace, User])],
@@ -17,6 +17,6 @@ import { User } from '../user/entities/user.entity';
 })
 export class ChannelModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(WorkspaceMiddleware).forRoutes(ChannelController);
+    consumer.apply(ChannelMiddleware).forRoutes(ChannelController);
   }
 }
